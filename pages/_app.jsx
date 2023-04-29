bf8dbba2e0447fc3e405ddf3c0c599cd3bddc3a0
Francois Huyghe <francois@13millieconds.com>
2023-04-28 20:10:54 -0400

    use Link instead of a

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { DatabaseProvider } from '../context/databaseContext'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }) {
   return <DatabaseProvider>
@@ -8,8 +9,8 @@ export default function App({ Component, pageProps }) {
         <h1>Seed to Fruit</h1>
       </div>
       <ul>
-        <li><a href="/">Plants</a></li>
-        <li><a href="/brands">Brands</a></li>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/brands">Brands</Link></li>
       </ul>
     </header>
     <Component {...pageProps} />
