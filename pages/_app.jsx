4b3f2d309454ec15d03ac07d311712e678ad687b
Francois Huyghe <francois@13millieconds.com>
2023-04-28 20:08:24 -0400

    reorganize page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,15 @@ import { DatabaseProvider } from '../context/databaseContext'
 
 export default function App({ Component, pageProps }) {
   return <DatabaseProvider>
+    <header>
+    <div className="z-10 w-full max-w-5xl items-center font-mono text-sm lg:flex">
+        <h1>Seed to Fruit</h1>
+      </div>
+      <ul>
+        <li><a href="/">Plants</a></li>
+        <li><a href="/brands">Brands</a></li>
+      </ul>
+    </header>
     <Component {...pageProps} />
   </DatabaseProvider>
 }
